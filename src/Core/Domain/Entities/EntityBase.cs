3d9e9c0d7b3d13a7cb42e6177e7e8e7fe87de4fb
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2023-07-13 04:56:50 +0400

    Create Architecture

---
+++ src/Core/Domain/Entities/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class EntityBase<T>
+    {
+        public T Id { get; set; } = default!;
+
+        public override string ToString()
+        {
+            return $"{GetType().Name}[Id={Id}]";
+        }
+    }
+}
